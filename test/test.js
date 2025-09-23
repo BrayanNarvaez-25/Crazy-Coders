2c8b802d150473c9d056b20e331b5497a9c0b447
BrayanNarvaez-25 <brayann251107g@gmail.com>
2025-09-22 21:14:02 -0500

    Se agrego h4 y más código para mostrar el resultado en pantalla

--- test/test.js
+++ test/test.js
@@ -2,5 +2,5 @@ saludar=function(){
     let name = recuperarTexto('txtNombre');
     let lastname = recuperarTexto('txtApellido');
     let saludo = generarSaludo(name, lastname);
-    console.log(saludo);
+    cambiarTexto("lblResultado",saludo);
 }
